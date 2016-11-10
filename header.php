96e422fcd5cc7ae1000a415faa775f120fc509f1
Daniel Karjanlahti <karjanlahti76@gmail.com>
2016-11-11 00:24:09 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,28 +4,28 @@
 <!--[if IE 7]><html class="no-js ie7"><![endif]-->
 <!--[if IE 8]><html class="no-js ie8"><![endif]-->
 <!--[if gt IE 8]><!-->
-<html class="no-js" <?php language_attributes(); ?>><!-- bara den här syns -->
+<html class="no-js" <?php language_attributes(); ?>>
     <!--<![endif]-->
     <head>
         <!--=== META TAGGAR ===-->
         <meta charset="utf-8">
-        <!-- säkerställer att webbläsare inte använder quirks mode -->
+        <!-- secures that the browser won't go into quirks mode -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         
-        <!--=== TITEL ===-->
+        <!--=== TITLE ===-->
         <title><?php if (!is_home()) { wp_title(); } else { echo 'Välkommen'; } ?> | <?php bloginfo('name'); ?></title>
 
         <meta name="description" content="<?php if (is_single()) { single_post_title('', true); } else { bloginfo('name'); echo " | "; bloginfo('description'); } ?>">
         <meta name="author" content="<?php echo AUTHOR; ?>">
 
-        <!-- Nästa tagg tar bort/återställer standardzoom på mobil enhet (iPad och iPhone) -->
+        <!-- Removes/resets standar dzoom iPad & iPhone) -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!--=== LÄNK TAGGAR ===-->
-        <!-- En länk till RSS-flödet på din webbplats. -->
+        <!--=== LINK TAGS ===-->
+        <!-- Link to RSS-feed for the site. -->
         <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS2 Feed" href="<?php bloginfo('rss2_url'); ?>">
 
-        <!-- En länk för Pingback URL för din webbplats -->
+        <!-- Link for Pingback URL for the site -->
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
         <script src="https://use.fontawesome.com/f22bfd3892.js"></script>
@@ -34,7 +34,7 @@
         <?php wp_head(); ?>
     </head>
 
-    <!-- body taggen har en hook som hjälper tema/plugin att styla med CSS -->
+    <!-- body tag hook which helps theme/plugin to style it -->
     <body <?php body_class($class); ?>> 
         <header class="banner">
             <div class="menu-wrapper">
@@ -45,7 +45,7 @@
                         <?php wp_nav_menu(array('theme_location' => 'main', 'container' => 'nav', 'container_class' => 'main-navigation')); ?>
                     </div>
                 </div>
-                <!-- Titel & Slogan -->
+                <!-- Title & description -->
                 <div class="site-name">
                     <h1 class="the-title"><a href='<?php echo esc_url(home_url('/')); ?>'><?php bloginfo('name'); ?></a></h1>
                     <h2 class="the-description"><?php bloginfo('description'); ?></h2>
