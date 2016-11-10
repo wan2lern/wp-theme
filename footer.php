ce294ad57767a87faaef0a40ba4cac6122cc2320
Daniel Karjanlahti <d@Daniels-MacBook-Pro.local>
2016-11-11 00:12:51 +0100

    Added main.css into which i moved the css from style.css + enqueued it in functions.php. Registered a menu and added theme_support for stuff in functions.php

--- footer.php
+++ footer.php
@@ -1,8 +1,17 @@
         </div>
         <footer>
-            <?php if (is_active_sidebar('bottom')) : ?>
-                <?php dynamic_sidebar('bottom'); ?>
-            <?php endif; ?>
+            <div class="credits">
+            <?php tc__f( 'tip' , __FUNCTION__ , __CLASS__, __FILE__ ); ?>
+                <?php
+                    $credits =  sprintf( '<p> &middot; &copy; %1$s <a href="%2$s" title="%3$s" rel="bookmark">%3$s</a> &middot; Designed by %4$s &middot;</p>',
+                            esc_attr( date( 'Y' ) ),
+                            esc_url( home_url() ),
+                            esc_attr(get_bloginfo()),
+                            '<a href="http://cr8gr8designs.com">cr8gr8designs.com</a>'
+                    );
+                    echo $credits;
+                ?>
+            </div>
         </footer>
     <?php wp_footer() ?>
     </body>
