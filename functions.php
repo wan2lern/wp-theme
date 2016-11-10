f7e7662eb0f617e6cd6f6201ab56aa6d65a9ccd9
Daniel Karjanlahti <karjanlahti76@gmail.com>
2016-11-11 00:51:52 +0100

    Update functions.php
    Set conditional on IE specific styles

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+define("THEME_DIR", get_template_directory_uri());
+
 // remove version info meta
 remove_action('wp_head', 'wp_generator');
 
@@ -11,15 +13,19 @@ add_theme_support('menus');
 // enqueue styles & scripts
 function mspecs_scripts() {
     // styles
-    wp_enqueue_style('main', get_stylesheet_uri() . '/css/main.css');
-    wp_enqueue_style('flexbox', THEME_DIR . '/css/flexbox.css');
+    wp_enqueue_style('mspecs-main', THEME_DIR . '/css/main.css');
+    wp_enqueue_style('mspecs-flexbox', THEME_DIR . '/css/flexbox.css');
     
-    // IE specific styles
-    wp_enqueue_style('ie', THEME_DIR . '/css/ie.css');
-    wp_enqueue_style('ie7', THEME_DIR . '/css/ie7.css');
+    // Load IE only stylesheet
+	wp_enqueue_style( 'mspecs-ie', THEME_DIR . "/css/ie.css", array( 'mspecs' ) );
+	wp_style_add_data( 'mspecs-ie', 'conditional', 'IE' );
+	
+    // Load IE7 specific stylesheet
+	wp_enqueue_style( 'mspecs-ie7', THEME_DIR . "/css/ie7.css", array( 'mspecs' ) );
+	wp_style_add_data( 'mspecs-ie7', 'conditional', 'IE 7' );
     
     // script
-    wp_enqueue_script('jquery', THEME_DIR . '/js/vendor/jquery.min.js', '3.1.1', true);
+    // wp_enqueue_script('jquery', THEME_DIR . '/js/vendor/jquery.min.js', '3.1.1', true);
     wp_enqueue_script('app', THEME_DIR . '/js/app.js', array('jquery'), '1.0.0', true);
 }
 
